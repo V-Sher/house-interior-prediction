db5ed3089ef522459100f921f92cbeec0118ddda
V-Sher <varshitasher@gmail.com>
2021-10-28 16:47:41 +0100

    added surname

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 
 # create a dict to populate the form; must be used when rendering the index.html
 myage = np.random.randint(10,100)
-personal_details = {'name': "Boooooooooo Sher", 
+personal_details = {'name': "Boooooooooo Sher Reddy", 
                     'age': myage}
 
 # basic first page
