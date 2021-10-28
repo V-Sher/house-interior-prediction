ba64328bf97e6bd3670ccf82dba4ff6399289730
V-Sher <varshitasher@gmail.com>
2021-10-28 18:07:49 +0100

    replace input from num to image file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 # import base64
 import numpy as np
 from flask import Flask, render_template, request
-# from PIL import Image
+from PIL import Image
 # from tensorflow.keras.models import load_model
 # from utils import model_config
 
@@ -29,9 +29,10 @@ def sayhello():
 
 @app.route('/dummy', methods=['POST'])
 def dummy():
-    num = int(request.form['num'])
-    image_shape = num*3
-    return render_template('index.html', personal_details=personal_details, img_shape=image_shape)
+    f = request.files['img']
+    file = Image.open(f)
+    file_shape = np.asarray(file).shape
+    return render_template('index.html', personal_details=personal_details, img_shape=file_shape)
 
 # # the name you give here in route will be reflected in URL
 # @app.route('/disp_size_myimg', methods=['POST'])
